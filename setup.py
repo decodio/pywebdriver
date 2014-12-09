8ffe00c24133a093bc777c170087754c8deec0d7
Sylvain Calador <sylvain.calador@gmail.com>
2014-12-09 15:50:08 +0100

    [IMP] Bump upstream version 0.1.2

--- setup.py
+++ setup.py
@@ -10,22 +10,9 @@ setup(
     description='Python Web Services to communicate wih Devices',
     license="AGPLv3+",
     long_description=open('README.md').read(),
-    package_dir={'pywebdriver': '.'},
-    packages=[
-        'pywebdriver',
-        'pywebdriver.libraries',
-        'pywebdriver.libraries.escpos',
-    ],
+    packages=find_packages(),
     install_requires=[ r.strip() for r in open('requirement.txt').read().splitlines() ],
-    package_data={'pywebdriver': [
-        'templates/*.html',
-        'static/images/*.png',
-        'translations/*/*/*',
-    ]},
-    entry_points={
-        'console_scripts': [
-            'pywebdriverd = pywebdriver.pywebdriver:main',
-    ]},
+    scripts = ['pywebdriverd'],
     include_package_data=True,
     zip_safe=False
 )
